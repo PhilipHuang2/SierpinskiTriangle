840d23f74c8b723ff73d4e03891a81cdb9794841
PhilipHuang2 <philiphuang2@gmail.com>
2015-01-15 11:19:51 -0800

    done

--- SierpinskiTriangle.pde
+++ SierpinskiTriangle.pde
@@ -1,16 +1,37 @@
+  int clock = 1;
 public void setup()
 {
-
+  size(800,800);
+  background(0, 0, 0);
 }
 public void draw()
 {
-
+	background(0,0,0);
+	fill(255, 255, 255);
+  	sierpinski(0,0,800,clock);
 }
 public void mouseDragged()//optional
 {
 
 }
-public void sierpinski(int x, int y, int len) 
+public void mousePressed()
 {
+ 	clock++;
+}
+public void sierpinski(int x, int y, int len, int count) 
+{
+  if( count <= 1 )
+  { 
+  	triangle(x, y, x + len, y, x + len/2, y + len);
+  }
+  else
+  { 
+  	
+  	
+    sierpinski(x, y ,len/2, count - 1); 
+    sierpinski(x + len/2, y, len/2, count -1);
+    sierpinski(x + len/4, y + len/2, len/2, count -1);
+
+  }
 
 }
---
+++ build-tmp/source/SierpinskiTriangle.java
@@ -0,0 +1,62 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
+  int clock = 1;
+public void setup()
+{
+  size(800,800);
+  background(0, 0, 0);
+}
+public void draw()
+{
+	background(0,0,0);
+	fill(255, 255, 255);
+  	sierpinski(0,0,800,clock);
+}
+public void mouseDragged()//optional
+{
+
+}
+public void mousePressed()
+{
+ 	clock++;
+}
+public void sierpinski(int x, int y, int len, int count) 
+{
+  if( count <= 1 )
+  { 
+  	triangle(x, y, x + len, y, x + len/2, y + len);
+  }
+  else
+  { 
+  	
+  	
+    sierpinski(x, y ,len/2, count - 1); 
+    sierpinski(x + len/2, y, len/2, count -1);
+    sierpinski(x + len/4, y + len/2, len/2, count -1);
+
+  }
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
